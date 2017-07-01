eea44a82ce5531b2334f6bb6ad62156d247f4892
Wilson Pilco Nuñez <wilsonaux1@gmail.com>
2017-06-30 22:12:32 -0500

    angular routes

--- resources/views/logistic/index.blade.php
+++ resources/views/logistic/index.blade.php
@@ -1,44 +1,104 @@
 <!DOCTYPE html>
 <html lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Cladesys - Logistic</title>
 
-        <link href="{{ asset('css/app.css') }} " rel="stylesheet">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cladesys - Logistic</title>
 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
+    <link href="{{ asset('css/app.css') }} " rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
-    </head>
-    <body>
-        <div ng-app="logistic" ng-controller="rootController">
-            
-            <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-                <a class="navbar-brand" href="{{ url('/') }} ">{{ config('app.name') }}</a>
+</head>
+
+<body>
+    <div ng-app="logistic" ng-controller="rootController">
+
+        <!--<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+                
                 <ul class="nav navbar-nav">
                     <li class="active">
                         <a class="navbar-brand" href="{{ url('/logistic') }} ">Logistic</a>
                     </li>
                     <li>
-                        <a href="#">Link</a>
+                        
                     </li>
                 </ul>
-            </nav>
-            
-            <div ng-view></div>
-        </div>
+            </nav>-->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                        aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="{{ url('/') }} ">{{ config('app.name') }}</a>
+                </div>
 
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="{{ url('/logistic') }} ">Logistic </a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="navbar-form navbar-left">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Link</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
         
-        <script src="{{ asset('js/app.js') }} "></script>
-        <script src="{{ asset('bower_components/angular/angular.js') }} "></script>
-        <script src="{{ asset('bower_components/angular-route/angular-route.js') }} "></script>
+        <div ng-view></div>
+    </div>
 
-        <script>window.url = "{{ url('') }}"</script>
-        <script src="{{ asset('js/logistic/main.js')}} "></script>
-    </body>
+
+    <script src="{{ asset('js/app.js') }} "></script>
+    <script src="{{ asset('bower_components/angular/angular.js') }} "></script>
+    <script src="{{ asset('bower_components/angular-route/angular-route.js') }} "></script>
+
+    <script>
+        window.url = "{{ url('') }}"
+    </script>
+    <script src="{{ asset('js/logistic/main.js')}} "></script>
+</body>
+
 </html>
